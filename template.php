b6a17c974af014fa9d3681941bcbe91fa2ad6471
Sivanandhan, P. <apsivam@92368.no-reply.drupal.org>
2007-10-11 17:24:08 +0000

    #166899 by jakemonO StumbleUpon patch by NaX

--- template.php
+++ template.php
@@ -4,18 +4,17 @@
 /**
  * @file
  * Example template.php for service_links.module
- * Use <?php print $service_links ?> to insert links in your node.tpl.php file.
+ * Use <?php print $service_links ?> to insert links in your node.tpl.php or you page.tpl.php file.
  */
 
 function _phptemplate_variables($hook, $vars) {
   switch($hook) {
     case 'node':
+    case 'page':
       if (module_exists('service_links')) {
         $vars['service_links'] = theme('links', service_links_render($vars['node'], TRUE));
       }
       break;
   }
-
   return $vars;
 }
-?>
