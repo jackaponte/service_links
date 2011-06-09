6a92e279215f0adf5fb24271af44c7b6b3a6e37c
Fabio Mucciante <thecrow@91238.no-reply.drupal.org>
2011-06-09 11:37:26 +0200

    Issue #1181612 by xxmuaddib, TheCrow: Fixed Facebook Share widget.

--- js/facebook_share.js
+++ js/facebook_share.js
@@ -1,7 +1,7 @@
 (function ($) {
   $(document).ready(function(){
     $('a.service-links-facebook-widget').each(function(){
-      $(this).attr('expr:share_url', $(this).attr('rel'));
+      $(this).attr('share_url', $(this).attr('rel'));
       $(this).attr('type', Drupal.settings.ws_fs.type);
       $(this).attr('name', 'fb_share');
     });
