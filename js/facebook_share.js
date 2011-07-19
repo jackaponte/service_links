069fb0b82049daf2a67b339e1ba5e86afda64484
Fabio Mucciante <thecrow@91238.no-reply.drupal.org>
2011-07-19 17:36:30 +0200

    by TheCrow: Added a counter fix for Facebook Share.

--- js/facebook_share.js
+++ js/facebook_share.js
@@ -6,5 +6,13 @@
       $(this).attr('type', Drupal.settings.ws_fs.type);
       $(this).attr('name', 'fb_share');
     });
+
+    if (Drupal.settings.ws_fs.fix_count) {
+      fix_no_count = function() {
+        $(".fb_share_no_count .fb_share_count_inner").text("0");
+        $(".fb_share_no_count").removeClass("fb_share_no_count");
+      }
+      var to = window.setTimeout(fix_no_count, 1500);
+    }
   });
 })(jQuery);
