738d314ebda1771bfd8003a00c9b6b1543df530a
Fabio Mucciante <thecrow@91238.no-reply.drupal.org>
2013-05-11 13:15:44 +0200

    Issue #1400724 by natuk, TheCrow: Fixed Facebook Like and Twitter button counter with https.

--- js/facebook_like.js
+++ js/facebook_like.js
@@ -2,12 +2,18 @@
   Drupal.behaviors.ws_fl = {
     attach: function (context, settings) {
       $('a.service-links-facebook-like', context).each(function(){
-        var iframe_txt='<iframe src="' + $(this).attr('href').replace('http://', '//') +
-          '" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:' +
-          Drupal.settings.ws_fl.width + 'px; height:' +
-          Drupal.settings.ws_fl.height + 'px;"' + ' allowTransparency="true"></iframe>';
- 
-        $(this).replaceWith(iframe_txt);
+        var iframe = document.createElement('iframe');
+
+        iframe.src = $(this).attr('href').replace('http://', '//').replace(/http[s]*\%3A\/\//, '');
+        iframe.setAttribute('scrolling', 'no');
+        iframe.setAttribute('frameborder', 0);
+        iframe.setAttribute('allowTransparency', 'true');
+
+        $(iframe).css('border', 'none').css('overflow', 'hidden')
+                 .css('width', Drupal.settings.ws_fl.width + 'px')
+                 .css('height', Drupal.settings.ws_fl.height + 'px');
+        
+        $(this).replaceWith(iframe);
       });
     }
   }
--- js/twitter_button.js
+++ js/twitter_button.js
@@ -6,6 +6,9 @@
       if (this.scriptadded) {
         twttr.widgets.load();
       } else {
+        $('a.service-links-twitter-widget', context).each(function(){
+          $(this).attr('href', $(this).attr('href').replace(/http[s]*\%3A\/\//, ''));
+        });
         $.getScript('http://platform.twitter.com/widgets.js', function () {
           this.scriptadded = true;
         });
