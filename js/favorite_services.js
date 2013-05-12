6f545347ffd1a01a1f3357e3654311cbf4d024ea
Fabio Mucciante <thecrow@91238.no-reply.drupal.org>
2013-05-12 10:26:09 +0200

    Issue #624364 by Peter Bex, AndyF: Fixed Favorite Service for IE 7 and 8.

--- js/favorite_services.js
+++ js/favorite_services.js
@@ -1,38 +1,40 @@
 (function ($) {
   Drupal.behaviors.sl_fs = {
     attach: function(context, settings) {
-      // It works only for FF, IE, Opera, Chrome
-      if (window.sidebar || window.external || window.opera) {
-        var $favorites = $("a.service-links-favorite", context).once('service-links-favorite');
-        $favorites.show();
-        if (window.chrome) {
-          $favorites.click(function(event){
-            event.preventDefault();
-            alert(Drupal.t('Use CTRL + D to add this to your bookmarks'));
-          });
-        } else if (window.opera && window.print) {
-          $favorites.each(function(){
-            var url = $(this).attr('href').split('&favtitle=');
-            var title = decodeURI(url[1]);
-            url = url[0];
-            $(this).attr('rel', 'sidebar').attr('href', url).attr('title', title);
-          });
-        } else if (window.sidebar || (window.external && window.external.AddFavourite)) {
-          $favorites.click(function(event){
-            event.preventDefault();
-            var url = $(this).attr('href').split('&favtitle=');
-            var title = decodeURI(url[1]);
-            url = url[0];
-            if (window.sidebar) {
-              window.sidebar.addPanel(title, url, '');
-            } else if (window.external) {
-              window.external.AddFavorite(url, title);
-            }
-          });
-        }
+      var $favorites = $("a.service-links-favorite", context).once('service-links-favorite');
+      var message = '';
+
+      $favorites.show();
+      if (window.chrome) {
+        message= Drupal.t('Use CTRL + D to add this to your bookmarks');
+      } else if (window.opera && window.print) {
+        $favorites.each(function(){
+          var url = $(this).attr('href').split('&favtitle=');
+          var title = decodeURI(url[1]);
+          url = url[0];
+          $(this).attr('rel', 'sidebar').attr('href', url).attr('title', title);
+        });
+      } else if (window.sidebar || window.external) {
+        $favorites.click(function(event){
+          event.preventDefault();
+          var url = $(this).attr('href').split('&favtitle=');
+          var title = decodeURI(url[1]);
+          url = url[0];
+          if (window.sidebar) {
+            window.sidebar.addPanel(title, url, '');
+          } else if (window.external) {
+            window.external.AddFavorite(url, title);
+          }
+        });
       } else {
-        $favorites.hide();
-      };
+        message = Drupal.t('Please use your browser to bookmark this page.');
+      }
+      if (message) {
+        $favorites.click(function(event){
+          event.preventDefault();
+          alert(message);
+        });
+      }
     }
   }
 })(jQuery);
