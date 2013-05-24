6521405e78e4a8f8c77452737fa2bff2b0d98a16
Fabio Mucciante <thecrow@91238.no-reply.drupal.org>
2013-05-24 12:57:08 +0200

    by wiifm, Raphael Apard, TheCrow: Updated the Google Plus One widget.

--- js/google_plus_one.js
+++ js/google_plus_one.js
@@ -3,25 +3,40 @@
     scriptadded: false,
 
     attach: function (context, settings) {
-      $('a.service-links-google-plus-one', context).each(function(){
-        var counter = Drupal.settings.ws_gpo.counter ? 'true' : 'false';
+      $('a.service-links-google-plus-one', context).once(function(){
         var g_text = document.createElement('g:plusone');
-        g_text.setAttribute('count', counter);
+
         g_text.setAttribute('href', $(this).attr('href'));
-        if (Drupal.settings.ws_gpo.type != '') {
-          g_text.setAttribute('size', Drupal.settings.ws_gpo.type);
+        g_text.setAttribute('width', Drupal.settings.ws_gpo.width);
+
+        if (Drupal.settings.ws_gpo.size != '') {
+          g_text.setAttribute('size', Drupal.settings.ws_gpo.size);
         }
+        if (Drupal.settings.ws_gpo.annotation != '') {
+          g_text.setAttribute('annotation', Drupal.settings.ws_gpo.annotation);
+        }
+        if (Drupal.settings.ws_gpo.callback) {
+          g_text.setAttribute('callback', Drupal.settings.ws_gpo.callback);
+        }
+
         $(this).replaceWith(g_text);
       });
 
       if (this.scriptadded) {
         gapi.plusone.go();
       } else {
+        var params = { parsetags: "explicit" };
+
+        if (Drupal.settings.ws_gpo.lang != '') {
+          params.lang = Drupal.settings.ws_gpo.lang;
+        }
+
+        window.___gcfg = params
+
         $.ajax({
           url: "https://apis.google.com/js/plusone.js",
           dataType: "script",
           cache: true,
-          parameters: { parsetags: "explicit" },
           success: function () {
             this.scriptadded = true;
             gapi.plusone.go();
